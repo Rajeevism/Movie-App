7a68207abb0a83f6e625acba5a64a5f758f6760d
Rajeevism <sundaravaradhan002@gmail.com>
2024-06-12 17:35:47 +0530

    Updated successfully

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from movie_app_controller import router as movie_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=movie_router, prefix="/movies", tags=["movies"])
 print(movie_router.routes)
